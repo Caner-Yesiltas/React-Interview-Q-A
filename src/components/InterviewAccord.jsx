b12b7e5a70e2eb1d92afe663a6216e9dde9bcb6c
CanerYesiltas <caneryesiltas1@gmail.com>
2024-09-07 21:22:44 -0400

    css styles added

--- src/components/InterviewAccord.jsx
+++ src/components/InterviewAccord.jsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react'
-import {arrowdown, arrowup} from "../helper/icons"
-const InterviewAccord = ({id, question, answer}) => {
+import React, { useState } from "react";
+import { arrowdown, arrowup } from "../helper/icons";
 
-    const [show,setShow] =useState(false);
-
-
+const InterviewAccord = ({ id, question, answer }) => {
+  const [show, setShow] = useState(false);
 
   return (
-    <div className='container'  >
-        <div className='question bg-warning m-2 ' >
-            <h2   >{id}.{question}</h2 > <span onClick={()=> setShow(!show)}>  {show? arrowup : arrowdown}  </span>
-        </div>
-      <div className='answer' >{show && answer} </div>
+    <div className="container  size" style={{ width: "90%", maxWidth: "50rem" }}>
+      <div className="question bg-warning m-2  ">
+        <h2>
+          {id}.{question}
+        </h2>{" "}
+        <span onClick={() => setShow(!show)}>
+          {" "}
+          {show ? arrowup : arrowdown}{" "}
+        </span>
+      </div>
+      <div className="answer ">{show && answer} </div>
     </div>
-  )
-}
+  );
+};
 
-export default InterviewAccord
+export default InterviewAccord;
--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -5,7 +5,7 @@ const MyHeader = () => {
   return (
     <div>
       <Container className='mt-5'>
-        <h1 className='text-danger text-center fs-1 '   > React Interview Questions And Answers</h1>
+        <h1 className='text-danger text-center fs-1 bg-dark mb-4  opacity-75 '   > React Interview Questions And Answers</h1>
       </Container>
     </div>
   )
